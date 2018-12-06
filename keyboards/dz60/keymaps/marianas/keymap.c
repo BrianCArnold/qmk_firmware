dd0058c973eb6db39b55c3d482979151f46cb7e8
Trenchie <trenchie@trenchie.us>
2018-12-06 13:41:05 -0500

    turned tab into CTRL when held.

--- keyboards/dz60/keymaps/marianas/keymap.c
+++ keyboards/dz60/keymaps/marianas/keymap.c
@@ -11,10 +11,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [QWERTY]=
     LAYOUT_marianas(
       ESCAP,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  MNUS,  EQUL, KC_DEL, BACKSPC,
-      KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  LBRC,  RBRC,  BSLASH,
+      MT(MOD_LCTL, KC_TAB),  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  LBRC,  RBRC,  BSLASH,
       FN_QT,    KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  COLN,  QUOT,  ENTER_OR_SQL,
       LEFTSHFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  CMMA,  PRRD,  SLSH,  RIGHT_SHIFT__PAREN,
-      CTLL,  WINL,  ALTL,  KC_SPC, KC_HYPR, KC_SPC,  ALTR,  WINR, KC_MEH, APPR,  CTLR),
+      CTLL,  WINL,  ALTL,  KC_SPC, LWIN(ESCAP), KC_SPC,  ALTR,  WINR, KC_MEH, APPR,  CTLR),
 
   [NAV_CLUSTER]=
     LAYOUT_60_ansi(
