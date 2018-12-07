7eca80e623391ca51d34a2af872db2d55613cff9
Trenchie <trenchie@trenchie.us>
2018-12-07 09:56:31 -0500

    Added center space to hit Win+Esc

--- keyboards/dz60/keymaps/marianas/keymap.c
+++ keyboards/dz60/keymaps/marianas/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  LBRC,  RBRC,  BSLASH,
       FN_QT,    KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  COLN,  QUOT,  ENTER_OR_SQL,
       LEFTSHFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  CMMA,  PRRD,  SLSH,  RIGHT_SHIFT__PAREN,
-      CTLL,  WINL,  ALTL,  KC_SPC, KC_HYPR, KC_SPC,  ALTR,  WINR, KC_MEH, APPR,  CTLR),
+      CTLL,  WINL,  ALTL,  KC_SPC, LWIN(ESCAP), KC_SPC,  ALTR,  WINR, KC_MEH, APPR,  CTLR),
 
   [NAV_CLUSTER]=
     LAYOUT_60_ansi(
